1c3045a747990e63d4e0271c1df956b3e4b4ac96
Mathieu Deous <mdeous@atheos.fr>
2013-01-01 18:24:17 +0100

    handle case where cam is already present in database

--- TrendnetStalker/lib/updater.py
+++ TrendnetStalker/lib/updater.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from sqlite3 import IntegrityError
+
 from TrendnetStalker import Camera
 from TrendnetStalker.lib.camscan import CamScanner
 
@@ -7,5 +9,9 @@ from TrendnetStalker.lib.camscan import CamScanner
 def update_cams(api_key):
     scanner = CamScanner(api_key)
     for url, lat, lng in scanner.get_cams():
-        cam = Camera.create(url=url, lat=lat, lng=lng)
-        print "Added new camera: %r" % url
+        try:
+            cam = Camera.create(url=url, lat=lat, lng=lng)
+        except IntegrityError:
+            continue
+        else:
+            print "Added new camera: %r" % url
